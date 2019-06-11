ca858fe8d72bfd64d9a5bcb047a837faaab5a881
ql <play-ql@gmail.com>
2019-06-12 03:44:32 +0800

    init

---
+++ objective-c-study/objective-c-study/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  objective-c-study
+//
+//  Created by killsos on 2019/6/12.
+//  Copyright © 2019 killsos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
